e118fd2b19f8e6039a83996454158a81aef652f2
wyy <Ellie_wyy@163.com>
2020-04-24 14:26:19 +0800

    Signed-off-by: wyy <Ellie_wyy@163.com> 2020年4月24日，将自己写的学习网站提交到远程

---
+++ 1.My Website/Untitled-1.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+	$cars=array(
+	  array("Volve",33,20),
+	  array("BMW",17,15),
+	  array("Saab",5,1),
+	  array("Land Rover",15,11),
+	);
+	for($row=0;$row<4;$row++){
+		echo "<p><b>行数 $row</b><p>";
+		echo "<ul>";
+		for($col=0;$col<3;$col++){
+			echo "<li>".$cars[$row][$col]."</li>";
+		}
+		echo "</ul>";
+	}
+?>
+</body>
+</html>
